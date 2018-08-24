8402c2e767a26d3621ae07881423fb6f458bb738
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-08-24 17:06:47 -0600

    hangin in there

--- main.js
+++ main.js
@@ -19,83 +19,79 @@ $(document).ready(function () {
 
     var words = ["jalepeno", "bladerunner", "bison", "screwdriver", "cat"]
     var letters = []
-    var guesses = 6
+    var guesses = 7
     var points = 0
+    
     var hangman = ['-----------------------------',
     '              |              ',
     '              0              ',
     '             /               ',
     '             /|              ',
-    '             /|\             ',
+    '             /|\\             ',
     '             /               ',
-    '             /\              ']
-
-    
-   function getGuess() {
-    $('#guess').on('change', function() {
-        guess = $('#guess').val()
-        guess = guess.split("")
-        if (guess.length > 1)
-         return console.log("Invalid Entry, Please Type a Single Letter")
-         else
-         return guess  
-    })
-}
+    '             /\\              ']
 
 
 
-    function getWord() {
-    letters = arrWord(words[Math.floor(Math.random() * words.length)])
-    console.log(letters)
-    return letters
+    function getGuess() {
+        $('#submit').on('click', function() {
+            guess  = $('#guess').val()
+            guess = guess.split("")
+            console.log(guess)
+            console.log(points)
+        })
     }
-    
-    function arrWord(wrd) {
-        return wrd.split("")
-        
-    }
-   function lettrCheck(){
-    $.each(letters, function(k,v) {
-        if (guess == v) {
-             points++
+
+        function getWord() {
+            letters = arrWord(words[Math.floor(Math.random() * words.length)])
+            console.log(letters)
+            return letters
         }
-        console.log(points); 
-      });
-    }
     
+        function arrWord(wrd) {
+            return wrd.split("")
+        
+        }
+
+
+        function lettrCheck(arr){
+            $.each(arr, function(k,v) {
+                if (guess == v) {
+                    points += 1
+                }else{
+                    points
+                }
+            })
+        }
     
-    function wordBoard() {
-        letters.forEach()
+
+        function wordBoard() {
+            letters.forEach()
             console.log("_")
-    }
+        }
+
 
     function drawPart(guesses) {
          switch(guesses){
          case 6:
-         return console.log(hangman[0])
+         return hangman[0]
          case 5:
-         return console.log(hangman[0] + "\n" + hangman[1] + "\n" + hangman[2])
+         return hangman[0] + "\n" + hangman[1] + "\n" + hangman[2]
          case 4:
-         return console.log(hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[3])
+         return hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[3]
          case 3:
-         return console.log(hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[4])
+         return hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[4]
          case 2:
-         return console.log(hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[5])
+         return hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[5]
          case 1:
-         return console.log(hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[5] + "\n" + hangman[6])
-         case 1:
-         return console.log(hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[5] + "\n" + hangman[7])
+         return hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[5] + "\n" + hangman[6]
+         case 0:
+         return hangman[0] + "\n" + hangman[1]+ "\n" + hangman[2] + "\n" + hangman[5] + "\n" + hangman[7]
          default:
-         return
+         return hangman[0]
          }
     }
 
-    $('#submit').on('click', function (){
-
-        getGuess()
-        getWord()
-        lettrCheck()
-            })
+});
 
 //********************************** */    
-    })
